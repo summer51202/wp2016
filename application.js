4177a905ec0285817fd928cf3af45da988964f71
summer51202 <summer51202@gmail.com>
2016-10-26 11:20:31 +0800

    Update application.js

--- application.js
+++ application.js
@@ -1,3 +1,10 @@
     $(document).ready(function() {
-     $('button').on('click', function() {});
-       });
+     $('button').on('click', function() {
+       var intro = $('
+         <p>興趣: 唱歌、攝影、喝咖啡</p>  
+         <p>喜好: 專注的感覺</p>     
+         <p>作品集: </p>');
+       $('.navbar').append(intro);
+       $('button').remove(); 
+     });
+    });
